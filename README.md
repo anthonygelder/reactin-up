40345a17a69fff6db617ef50d0af9150fb185583
Anthony Gelder <anthony.gelder@gmail.com>
2021-06-17 20:46:56 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Clone this repo to start a new project
+
+git clone CLONE-URL test-project && cd $_
+
+rm -rf .git && git init
+
+Create to repo on GitHub
+
+commit 
+
+add origin
+
+push master
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import { Route, Link } from 'react-router-dom'
+import './App.css'
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default App;
